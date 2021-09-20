29621a34530a1d0173d161401e12d8cfd7b053a6
srgooglo <srgooglo@ragestudio.net>
2021-09-20 12:09:55 +0200

    use corenode-node

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,27 +1,24 @@
-#!/usr/bin/env node
-const corenode = require("corenode")
+#!/usr/bin/env corenode-node
 
-corenode.runInNewRuntime(() => {
-    const { Command, program } = require("commander")
-    const { EviteServer } = require("../server/index.js")
+const { Command, program } = require("commander")
+const { EviteServer } = require("../server/index.js")
 
-    const cliHandler = {
-        dev: async (entry) => {
-            let server = await new EviteServer({
-                entry
-            }).initialize()
+const cliHandler = {
+    dev: async (entry) => {
+        let server = await new EviteServer({
+            entry
+        }).initialize()
 
-            await server.listen()
-        },
-        build: () => {
-            console.error("Build isn`t already available")
-        }
+        await server.listen()
+    },
+    build: () => {
+        console.error("Build isn`t already available")
     }
+}
 
-    const devCMD = new Command("dev", "Runs the development server").arguments("[entry]").action(cliHandler.dev)
-    const buildCMD = new Command("build", "Build with vite").arguments("[entry]").action(cliHandler.build)
+const devCMD = new Command("dev", "Runs the development server").arguments("[entry]").action(cliHandler.dev)
+const buildCMD = new Command("build", "Build with vite").arguments("[entry]").action(cliHandler.build)
 
-    program.addCommand(devCMD)
-    program.addCommand(buildCMD)
-    program.parse()
-})
+program.addCommand(devCMD)
+program.addCommand(buildCMD)
+program.parse()
