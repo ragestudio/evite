8798db877f31a83c09731feb66d8db1f7de6b672
srgooglo <srgooglo@ragestudio.net>
2021-09-24 15:06:28 +0200

    support context injection

--- src/server/renderers/react.js
+++ src/server/renderers/react.js
@@ -1,23 +1,30 @@
 const { TemplateGenerator } = require("../../lib")
 
-module.exports = (params = {}) => {
+module.exports = (params = {}, context) => {
   if (!params.main) {
     throw new Error(`Missing MainModule`)
   }
 
   const template = new TemplateGenerator()
 
+  // support context injection
+  if (Array.isArray(context)) {
+    context.forEach(line => {
+      template.line(line)
+    })
+  }
+
   // append basics of react
-  template.appendImport("React", "react")
-  template.appendImport("ReactDOM", "react-dom")
-  template.appendImport("{ BrowserRouter }", "react-router-dom")
+  template.import("React", "react")
+  template.import("ReactDOM", "react-dom")
+  template.import("{ BrowserRouter }", "react-router-dom")
 
   // import main 
-  template.appendImport("__MainModule", params.main)
+  template.import("__MainModule", params.main)
 
-  template.appendConstable("__Main", `() => { return <BrowserRouter> <__MainModule /> </BrowserRouter> }`)
-  template.appendFunction("__createRender", undefined, `return ReactDOM.render(<__Main />, document.getElementById("root"))`)
-  template.appendCall("__createRender")
+  template.constable("__Main", `() => { return <BrowserRouter> <__MainModule /> </BrowserRouter> }`)
+  template.function("__createRender", undefined, `return ReactDOM.render(<__Main />, document.getElementById("root"))`)
+  template.call("__createRender")
 
   return template.construct()
 }
