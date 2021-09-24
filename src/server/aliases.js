b2a117e43fe84e882fb0914b1da0e3403babb757
srgooglo <srgooglo@ragestudio.net>
2021-09-24 09:44:47 +0200

    added defaultAliases controller

---
+++ src/server/aliases.js
@@ -0,0 +1,33 @@
+const path = require("path")
+
+function getDefaultAliases(cwd) {
+    const rootPath = cwd ?? process.cwd()
+    const sourcePath = path.resolve((cwd ?? rootPath), "src")
+
+    let rootAliases = {
+        "$": rootPath,
+        schemas: path.join(rootPath, 'schemas'),
+        interface: path.join(rootPath, 'interface'),
+        config: path.join(rootPath, 'config'),
+    }
+
+    let appAliases = {
+        "@": sourcePath,
+        extensions: path.join(sourcePath, 'extensions'),
+        theme: path.join(sourcePath, 'theme'),
+        locales: path.join(sourcePath, 'locales'),
+        core: path.join(sourcePath, 'core'),
+        pages: path.join(sourcePath, 'pages'),
+        components: path.join(sourcePath, 'components'),
+        models: path.join(sourcePath, 'models'),
+    }
+
+    return {
+        ...appAliases,
+        ...rootAliases
+    }
+}
+
+module.exports = {
+    getDefaultAliases
+}
