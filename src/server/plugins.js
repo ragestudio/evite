b783804b3e25a0d0a3729ae548b45410cd6a29a5
srgooglo <srgooglo@ragestudio.net>
2021-09-27 09:30:38 +0200

    disable vite react plugin

--- src/server/plugins.js
+++ src/server/plugins.js
@@ -2,7 +2,6 @@
 
 function getDefaultPlugins() {
     return [
-        require( "vite-plugin-require").default(),
         require("@vitejs/plugin-react-refresh")(),
         require('@rollup/plugin-node-resolve').default({
             browser: true
