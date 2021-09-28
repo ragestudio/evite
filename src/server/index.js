6e1165e5bb066856b655bead2360fe7079436d3f
srgooglo <srgooglo@ragestudio.net>
2021-09-28 10:16:36 +0200

    renamed exports

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,11 @@
 const { DevelopmentServer } = require('./servers/base.js')
 const { SSRServer, SSRReactServer, createSSRReactServer } = require('./servers/ssr.js')
-const { ViteDevelopmentServer } = require('./servers/vite.js')
+const { ReactViteDevelopmentServer } = require('./servers/vite.js')
 
 module.exports = {
     DevelopmentServer,
     SSRServer,
     SSRReactServer,
     createSSRReactServer,
-    ViteDevelopmentServer,
+    ReactViteDevelopmentServer,
 }
