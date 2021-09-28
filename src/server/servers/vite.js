78028ad665cdbbad46f68fd7b92a14206833b30a
srgooglo <srgooglo@ragestudio.net>
2021-09-28 10:17:21 +0200

    support custom http serving

--- src/server/servers/vite.js
+++ src/server/servers/vite.js
@@ -1,28 +1,52 @@
 const vite = require('vite')
 const { DevelopmentServer } = require('./base')
+const { compileIndexHtmlTemplate } = require("../../lib")
+const buildReactTemplate = require("../renderers/react")
+const express = require('express')
 
-class ViteDevelopmentServer extends DevelopmentServer {
-    constructor(params) {
-        super(params)
-
-        this.config.server.middlewareMode = undefined
-        process.env.__DEV_MODE_SSR = 'false'
-
-        return this
-    }
-
+class ReactViteDevelopmentServer extends DevelopmentServer {
     initialize = async () => {
-        await this.compileDefinitions()
-
+        const http = express()
+        const definitions = await this.compileDefinitions()
         const instanceConfig = {
             ...this.config,
             root: this.cwd,
+            server: {
+                ...this.config.server,
+                middlewareMode: 'ssr'
+            },
         }
+        const server = await vite.createServer(instanceConfig)
 
-        return await vite.createServer(instanceConfig)
+        http.use(server.middlewares)
+        http.use('*', async (req, res) => {
+            const isRedirect = ({ status = 0 } = {}) => status >= 300 && status < 400
+            const protocol = req.protocol || (req.headers.referer || '').split(':')[0] || 'http'
+            const url = protocol + '://' + req.headers.host + req.originalUrl
+
+            try {
+                const template = await buildReactTemplate({ main: this.entry }, [definitions]).write()
+                const indexHtml = await server.transformIndexHtml(url, compileIndexHtmlTemplate(template.file.output))
+
+                return res.status(200).end(indexHtml)
+            } catch (error) {
+                server.ssrFixStacktrace(error)
+                console.error(error)
+                res.status(500).end(error.message)
+            }
+        })
+
+        return {
+            http,
+            server,
+            listen: () => {
+                this.events.emit("server_listen")
+                return http.listen(this.config.server?.port ?? 8000)
+            }
+        }
     }
 }
 
 module.exports = {
-    ViteDevelopmentServer
+    ReactViteDevelopmentServer
 }
