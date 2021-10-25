13fafb4e2b558ab4b3ea33f5467557eae30df4a0
srgooglo <srgooglo@ragestudio.net>
2021-10-25 12:06:11 +0200

    refactor compileIndex

--- src/lib/compileIndexHtmlTemplate/index.js
+++ src/lib/compileIndexHtmlTemplate/index.js
@@ -1,8 +1,22 @@
-export default (mainScript) => {
-    return `<!DOCTYPE html>
+export default (payload = {}) => {
+  const { head, mainScript } = payload
+  let headScripts = []
+  
+  if (Array.isArray(head)) {
+    head.forEach(script => {
+      headScripts.push(script)
+    })
+  }
+
+  function getHeadScripts() {
+    return headScripts.map(script => `<script type="module" src="${script}"></script>`).join('\n')
+  }
+
+  return `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8" />
+    ${getHeadScripts()}
     <link rel="icon" type="image/svg+xml" href="src/assets/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   </head>
@@ -10,7 +24,6 @@ export default (mainScript) => {
     <div id="root">
     <!--app-html-->
     </div>
-    <script type="module" src="${mainScript}"></script>
   </body>
 </html>`
 }
