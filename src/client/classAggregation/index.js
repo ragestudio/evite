3e79db166fb0da11b359a2baaea92078d4fc5143
srgooglo <srgooglo@ragestudio.net>
2021-10-26 09:01:15 +0200

    refactor

--- src/client/classAggregation/index.js
+++ src/client/classAggregation/index.js
@@ -1,28 +1,55 @@
-export default (baseClass, ...mixins) => {
-    class base extends baseClass {
+/*
+**  Aggregation -- Aggregation of Base Class and Mixin Classes
+**  Copyright (c) 2015-2021 Dr. Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+export default (base, ...mixins) => {
+    let aggregate = class __Aggregate extends base {
         constructor(...args) {
             super(...args)
+
             mixins.forEach((mixin) => {
-                copyProps(this, new mixin())
+                if (typeof mixin.prototype.initializer === "function") {
+                    mixin.prototype.initializer.apply(this, args)
+                }
             })
         }
     }
 
     let copyProps = (target, source) => {
-        // this function copies all properties and symbols, filtering out some special ones
         Object.getOwnPropertyNames(source)
             .concat(Object.getOwnPropertySymbols(source))
             .forEach((prop) => {
-                if (!prop.match(/^(?:constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/))
-                    Object.defineProperty(target, prop, Object.getOwnPropertyDescriptor(source, prop))
+                if (prop.match(/^(?:initializer|constructor|prototype|arguments|caller|name|bind|call|apply|toString|length)$/)) {
+                    return
+                }
+                Object.defineProperty(target, prop, Object.getOwnPropertyDescriptor(source, prop))
             })
     }
 
     mixins.forEach((mixin) => {
-        // outside constructor() to allow aggregation(A,B,C).staticFunction() to be called etc.
-        copyProps(base.prototype, mixin.prototype)
-        copyProps(base, mixin)
+        copyProps(aggregate.prototype, mixin.prototype)
+        copyProps(aggregate, mixin)
     })
 
-    return base
+    return aggregate
 }
