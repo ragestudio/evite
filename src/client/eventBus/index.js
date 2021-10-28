7cc474c9046bf8a7e94320982b3e1bece47fbc07
srgooglo <srgooglo@ragestudio.net>
2021-10-28 12:43:33 +0200

    emit debug

--- src/client/eventBus/index.js
+++ src/client/eventBus/index.js
@@ -1,12 +1,22 @@
 import EventEmitter from "@foxify/events"
 
 export default class EventBus extends EventEmitter {
-    constructor() {
-        super({ captureRejections: true })
+    constructor(params = {}) {
+        super({ ...params, captureRejections: true })
+
+        this.id = params?.id
     }
+
+    _emit = this.emit
+
+    emit = (event, ...args) => {
+        console.debug(`[EventBus][emit *${event}*]`)
+        return this._emit(event, ...args)
+    }
+
     on = (event, listener, context) => {
         const _listener = (...args) => {
-            console.debug(`Event [${event}] resolved`)
+            console.debug(`[EventBus][on *${event}*]`)
             listener(...args)
         }
         return this._addListener(event, _listener, context, true, false)
