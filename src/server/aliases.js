9789e82b0b0f16f9c463a532e84aa2ba675e34dc
srgooglo <srgooglo@ragestudio.net>
2021-12-09 08:56:44 +0100

    clean up aliases

--- src/server/aliases.js
+++ src/server/aliases.js
@@ -4,25 +4,9 @@ function getDefaultAliases(cwd) {
     const rootPath = cwd ?? process.cwd()
     const sourcePath = path.resolve((cwd ?? rootPath), "src")
 
-    let rootAliases = {
-        "$": rootPath,
-        config: path.join(rootPath, 'config'),
-    }
-
-    let appAliases = {
-        "@": sourcePath,
-        extensions: path.join(sourcePath, 'extensions'),
-        theme: path.join(sourcePath, 'theme'),
-        locales: path.join(sourcePath, 'locales'),
-        core: path.join(sourcePath, 'core'),
-        pages: path.join(sourcePath, 'pages'),
-        components: path.join(sourcePath, 'components'),
-        models: path.join(sourcePath, 'models'),
-    }
-
     return {
-        ...appAliases,
-        ...rootAliases
+        "$": rootPath,
+        "@": sourcePath,
     }
 }
 
