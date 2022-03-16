6816031bfd017bf158e0531217171226219d4402
srgooglo <srgooglo@ragestudio.net>
2022-03-16 01:38:08 +0100

    implement extension class

--- packages/lib/src/extension/index.js
+++ packages/lib/src/extension/index.js
@@ -1,6 +1,34 @@
+import { Observable } from "object-observer"
+
 export default class EviteExtension {
-    constructor(params = {}) {
-        this.params = params
+    constructor(appContext, mainContext) {
+        this.appContext = appContext
+        this.mainContext = mainContext
+
+        this.promises = []
+
+        return this
     }
-    
+
+    initializer() {
+        return new Promise(async (resolve, reject) => {
+            if (Array.isArray(this.depends)) {
+                this.depends.forEach((dependency) => {
+                    const dependencyPromise = new Promise((resolve, reject) => {
+                        Observable.observe(this.mainContext.ATTACHED_EXTENSIONS, (changes) => {
+                            changes.forEach((change) => {
+                                Array.from(change.object).includes(dependency) && resolve()
+                            })
+                        })
+                    })
+
+                    this.promises.push(dependencyPromise)
+                })
+            }
+
+            await Promise.all(this.promises)
+
+            return resolve()
+        })
+    }
 }
